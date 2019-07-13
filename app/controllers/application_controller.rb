8ab5594acb947a2f26a1ee81c3e4df511bb83b64
Jared Grove <jaredkgrove@gmail.com>
2019-07-13 18:17:54 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,7 +22,7 @@ class ApplicationController < Sinatra::Base
 			redirect "/login"
 		else
 			redirect "/failure"
-end
+		end
 	end
 
 	get "/login" do
@@ -30,7 +30,7 @@ end
 	end
 
 	post "/login" do
-		#your code here!
+	  user = User.find_by(:username => params[:username])
 	end
 
 	get "/success" do
